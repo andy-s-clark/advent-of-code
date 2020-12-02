855640ed58a7ba30217712700ca04e57e9faefd4
Andy Clark <4296315+andy-s-clark@users.noreply.github.com>
2020-12-01 16:37:44 -0800

    Puzzle day 1 improvements

--- 2020/puzzle_1a.py
+++ 2020/puzzle_1a.py
@@ -4,10 +4,13 @@ with open("puzzle_1.txt", "r") as f:
     for line in f:
         num_list.append(int(line.strip()))
 
-for i in num_list:
-    for j in num_list:
-        if i != j and i+j == target_sum:
-            print("%i * %i = %i" % (i, j, i*j))
+list_len = len(num_list)
+
+for i in range(0, list_len):
+    for j in range(1, list_len):
+        if i != j and num_list[i]+num_list[j] == target_sum:
+            print("%i * %i = %i"
+                  % (num_list[i], num_list[j], num_list[i]*num_list[j]))
             exit(0)
 
 print("Failed")
--- 2020/puzzle_1b.py
+++ 2020/puzzle_1b.py
@@ -4,11 +4,14 @@ with open("puzzle_1.txt", "r") as f:
     for line in f:
         num_list.append(int(line.strip()))
 
-for i in num_list:
-    for j in num_list:
-        for k in num_list:
-            if i != j and i != k and j != k and i+j+k == target_sum:
-                print("%i * %i * %i = %i" % (i, j, k, i*j*k))
+list_len = len(num_list)
+
+for i in range(0, list_len):
+    for j in range(1, list_len):
+        for k in range(2, list_len):
+            if i != j and i != k and j != k and num_list[i]+num_list[j]+num_list[k] == target_sum:
+                print("%i * %i * %i = %i"
+                      % (num_list[i], num_list[j], num_list[j], num_list[i]*num_list[j]*num_list[k]))
                 exit(0)
 
 print("Failed")
