56c5d743e74eb38d706fea72b230fb737f13f9ad
Andy Clark <4296315+andy-s-clark@users.noreply.github.com>
2020-12-08 21:28:41 -0800

    Day 9 puzzle 1

---
+++ 2020/puzzle_9a.py
@@ -0,0 +1,31 @@
+def load_data(filename):
+    d = []
+    with open(filename, 'r') as f:
+        for line in f:
+            d.append(int(line.strip()))
+    return d
+
+
+def find_first_outlier(data, preamble_length):
+    ps = []
+    for i in range(0, len(data) - 1):
+        if i >= preamble_length:
+            if not is_valid_number(data[i], ps):
+                return data[i]
+            ps.pop(0)
+        ps.append(data[i])
+    return None
+
+
+def is_valid_number(num, predecessors):
+    ps = set(predecessors)
+    for i in ps:
+        for j in ps:
+            if i+j == num:
+                return True
+    return False
+
+
+my_data = load_data('puzzle_9.txt')
+my_outlier = find_first_outlier(my_data, 25)
+print(my_outlier)
