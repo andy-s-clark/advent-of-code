5fb2ffb3f57b72fdeedde162f224007320c21965
Andy Clark <4296315+andy-s-clark@users.noreply.github.com>
2020-12-10 14:47:50 -0800

    Day 10 puzzle 2
    Semi-manual process. Could have been automated more, but it was quicker
    to separate out the different lists using a spreadsheet.
    The list was split whenever there were two consecutive diffs of 3. For
    simplicity the data was then adjusted in the sublist to treat the first
    item as the 0 starting point.

--- 2020/puzzle_10b.py
+++ 2020/puzzle_10b.py
@@ -1,41 +1,12 @@
-import logging
-
-
 def read_adapters(filename):
     adapters = []
     with open(filename, 'r') as f:
         for line in f:
-            adapters.append(int(line.strip()))
+            try:
+                adapters.append(int(line.strip()))
+            except ValueError:
+                continue
     return adapters
-
-
-def get_differences(adapters, start):
-    ds = []
-    last = start
-    for a in set(adapters):
-        ds.append((a, a - last))
-        last = a
-    return ds
-
-
-def get_unique_adapter_sets(adapters, start):
-    uass = []
-    uas = []
-    last_diff = 0
-    diffs = get_differences(adapters + [start], start)
-    logging.debug('Diffs %s:' % diffs)
-    for ds in diffs:
-        if ds[1] < 3:
-            uas.append(ds[0])
-        elif last_diff < 3:
-            uas.append(ds[0])
-            uass.append(uas)
-            uas = [ds[0]]
-        else:
-            logging.debug('Dropped consecutive 3: %i' % ds[0])
-        last_diff = ds[1]
-    uass.append(uas)
-    return uass
 
 
 def is_valid_adapter(input_joltage, adapter):
@@ -55,18 +26,12 @@ def get_valid_combinations(adapters, start, end):
     return combinations
 
 
-logging.basicConfig(level=logging.DEBUG)
-my_adapters = read_adapters('puzzle_10_sample_b.txt')
-logging.debug('Adapters: %s' % sorted(my_adapters))
-logging.debug('Answer using recursion for the lot: %s' % len(get_valid_combinations(my_adapters, 0, max(my_adapters))))
-my_adapter_sets = get_unique_adapter_sets(my_adapters, 0)
-logging.debug('Adapter Sets: %s' % my_adapter_sets)
-total_combos = 1
-for my_as in my_adapter_sets:
-    if len(my_as) > 3:
-        my_start = my_as.pop(0)
-        my_end = my_as.pop()
-        combo_count = len(get_valid_combinations(my_as, my_start, my_end))
-        total_combos = total_combos * (combo_count if combo_count > 1 else 1)
-        logging.debug('Combo Count: %i, Start: %i, Adapter Set: %s, End: %i' % (combo_count, my_start, my_as, my_end))
-print(total_combos)
+total_combinations = 1
+for i in range(1, 7):
+    my_adapters = read_adapters("puzzle_10_data/%i.txt" % i)
+    my_start = 0
+    my_end = max(my_adapters) + 3
+    my_combinations = get_valid_combinations(sorted(my_adapters), my_start, my_end)
+    print(i, len(my_combinations))
+    total_combinations = total_combinations * len(my_combinations)
+print(total_combinations)
